0f7af387bb1bd781fe23d04226379235e848956f
Gurmeet Raj <126115742+gURmeet2003@users.noreply.github.com>
2026-06-28 14:15:16 +0530

    Delete VendorService directory

--- VendorService/src/main/java/com/training/dao/VendorDAO.java
+++
@@ -1,9 +0,0 @@
-package com.training.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.training.entity.VendorEntity;
-
-public interface VendorDAO extends JpaRepository<VendorEntity, String> {
-
-}
